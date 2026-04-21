df27d2ef0e7f5a69a7ddd6ae2ecf812d4f7e7118
Darryl Wagoner <darryl@wagoner.me>
2026-04-21 13:04:58 -0500

    using treeview

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -2,5 +2,63 @@
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public MenuNode[] MenuItems { get; } =
+    [
+        new MenuNode("File", true,
+            new MenuNode("Import ADIF"),
+            new MenuNode("Export ADIF"),
+            new MenuNode("Remove Dups"),
+            new MenuNode("Watch List")),
+        new MenuNode("Edit"),
+        new MenuNode("Settings"),
+        new MenuNode("Callbook"),
+        new MenuNode("List"),
+        new MenuNode("Search"),
+        new MenuNode("Awards"),
+        new MenuNode("eLogs"),
+        new MenuNode("RecCall"),
+        new MenuNode("Net View"),
+        new MenuNode("Help")
+    ];
+
+    private MenuNode? _selectedMenuItem;
+
+    public MainWindowViewModel()
+    {
+        SelectedMenuItem = MenuItems[0];
+    }
+
+    public MenuNode? SelectedMenuItem
+    {
+        get => _selectedMenuItem;
+        set
+        {
+            if (SetProperty(ref _selectedMenuItem, value))
+            {
+                OnPropertyChanged(nameof(SelectedMenuTitle));
+            }
+        }
+    }
+
+    public string SelectedMenuTitle => SelectedMenuItem?.Title ?? "None";
+}
+
+public sealed class MenuNode
+{
+    public MenuNode(string title, params MenuNode[] children)
+        : this(title, false, children)
+    {
+    }
+
+    public MenuNode(string title, bool isExpanded, params MenuNode[] children)
+    {
+        Title = title;
+        IsExpanded = isExpanded;
+        Children = children;
+    }
+
+    public string Title { get; }
+    public bool IsExpanded { get; set; }
+    public MenuNode[] Children { get; }
+    public bool HasChildren => Children.Length > 0;
 }
