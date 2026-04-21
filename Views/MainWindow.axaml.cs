951437950cb3a1a439fcea6f97d59c2b2d407a6f
Darryl Wagoner <darryl@wagoner.me>
2026-04-21 13:57:33 -0500

    change Treeview to single click

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using HamBusLog.ViewModels;
+using Avalonia.VisualTree;
 
 namespace HamBusLog.Views;
 
@@ -7,5 +10,28 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void OnTreeViewItemPressed(object? sender, PointerPressedEventArgs e)
+    {
+        // Placeholder - will be replaced with OnTreeViewPointerPressed
+    }
+
+    public void OnTreeViewPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        var treeViewItem = e.Source as TreeViewItem ?? (e.Source as Control)?.FindAncestorOfType<TreeViewItem>();
+        if (treeViewItem != null && treeViewItem.DataContext is MenuNode node && node.HasChildren)
+        {
+            treeViewItem.IsExpanded = !treeViewItem.IsExpanded;
+        }
+    }
+
+    public void OnTreeViewItemPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (sender is TreeViewItem treeViewItem && treeViewItem.DataContext is MenuNode node && node.HasChildren)
+        {
+            treeViewItem.IsExpanded = !treeViewItem.IsExpanded;
+            e.Handled = true;
+        }
     }
 }
